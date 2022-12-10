335663a55801e7edd11cfed8ee6608fd608045eb
TimA314 <ci0d7j9m@mailer.me>
2022-12-09 21:40:05 -0800

    set up experience and contact

---
+++ src/components/Education.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Experience.css'; 
+
+const Experience = () => {  
+  return (
+    <div className="experience">
+      <h2>Experience</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Experience;
